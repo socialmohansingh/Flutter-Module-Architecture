3e97d5b3f5c6af5f9da64e535a191b460a6888ca
Mohan Singh Thagunna <mohan.thagunna.ext@deliveryhero.com>
2022-12-24 23:59:36 +0545

    initial pages added

--- lib/src/navigation/root_navigation_widget.dart
+++ lib/src/navigation/root_navigation_widget.dart
@@ -3,8 +3,11 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_module_architecture/src/navigation/navigation_cubit.dart';
 import 'package:flutter_module_architecture/src/navigation/navigation_state.dart';
 
+// ignore: must_be_immutable
 class RootNavigatorWidget extends StatefulWidget {
-  const RootNavigatorWidget({
+  List<Page<dynamic>> initialPages;
+  RootNavigatorWidget({
+    required this.initialPages,
     super.key,
   });
 
@@ -21,7 +24,8 @@ class _RootNavigatorWidgetState extends State<RootNavigatorWidget> {
       create: (context) => NavigationCubit(InitialState([])),
       child: BlocBuilder<NavigationCubit, NavigationState>(
         builder: (context, state) {
-          List<Page<dynamic>> pages = state.pages;
+          List<Page<dynamic>> pages =
+              state.pages.isEmpty ? widget.initialPages : state.pages;
           return Navigator(
             pages: List.unmodifiable(pages),
             observers: [heroController],
